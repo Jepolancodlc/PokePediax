00d493d45565efc72ffb6d74a5387081c960420a
John Polanco <jepolancodlc@gmail.com>
2024-05-18 21:35:18 +0200

    "Updated card component HTML, SCSS, and TS files; moved type color logic to svc-filter service; and adjusted home component SCSS padding."

--- PokePediax/src/app/Components/card/card.component.ts
+++ PokePediax/src/app/Components/card/card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, SimpleChanges } from '@angular/core';
 import { Resultado } from '../../Interfaces/pokeApi';
 import { PokemonService } from '../../Services/pokemon.service';
 import { Pokemon } from '../../Interfaces/pokemon';
+import { SvcFilterService } from '../../Services/svc-filter.service';
 
 @Component({
   selector: 'app-card',
@@ -14,57 +15,10 @@ export class CardComponent {
   idPadStart = '0';
   pokemonSelect?: Pokemon;
 
+  constructor(public svcFilterService: SvcFilterService) {}
+
   ngOnChanges(changes: SimpleChanges): void {
     this.id = this.data?.id;
     this.idPadStart = (this.id ?? '').toString().padStart(4, '0');
-  }
-  getColorForType(type: string): string {
-    switch (type) {
-      case 'grass':
-        return '#7ED957'; // Green
-      case 'fire':
-        return '#FF914D'; // Orange
-      case 'water':
-        return '#38B6FF'; // Blue
-      case 'bug':
-        return '#3E721D'; // Green
-      case 'normal':
-        return '#AFAFAD'; // Gray
-      case 'poison':
-        return '#8C52FF'; // Purple
-      case 'electric':
-        return '#EFCA35'; // Yellow
-      case 'ground':
-        return '#9B5635'; // Brown
-      case 'fairy':
-        return '#FFC0CB'; // Pink
-      case 'fighting':
-        return '#FF3131'; // Brown
-      case 'psychic':
-        return '#CB6CE6'; // Purple
-      case 'rock':
-        return '#B2935B'; // Brown
-      case 'steel':
-        return '#737373'; // Gray
-      case 'ice':
-        return '#5CE1E6'; // Light blue
-      case 'ghost':
-        return '#3A2366'; // Purple
-      case 'dragon':
-        return '#BEDD8B'; // Purple
-      case 'dark':
-        return '#393838'; // Black
-      case 'flying':
-        return '#0097B2'; // Sky blue
-      default:
-        return '#AFAFAD'; // Gray (Color por defecto si el tipo no coincide)
-    }
-  }
-  getColorForTypes(types: string[]): string[] {
-    if (types.length === 1) {
-      return [this.getColorForType(types[0]), this.getColorForType(types[0])];
-    } else {
-      return types.map((type) => this.getColorForType(type));
-    }
   }
 }
--- PokePediax/src/app/Services/svc-filter.service.ts
+++ PokePediax/src/app/Services/svc-filter.service.ts
@@ -91,4 +91,47 @@ export class SvcFilterService {
       of(this.tiposSelecionados),
     ]).pipe(map(([value, _]) => this._filter(value || '')));
   }
+
+  getColorForType(type: string): string {
+    switch (type) {
+      case 'grass':
+        return '#7ED957'; // Green
+      case 'fire':
+        return '#FF914D'; // Orange
+      case 'water':
+        return '#38B6FF'; // Blue
+      case 'bug':
+        return '#3E721D'; // Green
+      case 'normal':
+        return '#AFAFAD'; // Gray
+      case 'poison':
+        return '#8C52FF'; // Purple
+      case 'electric':
+        return '#EFCA35'; // Yellow
+      case 'ground':
+        return '#9B5635'; // Brown
+      case 'fairy':
+        return '#FFC0CB'; // Pink
+      case 'fighting':
+        return '#FF3131'; // Brown
+      case 'psychic':
+        return '#CB6CE6'; // Purple
+      case 'rock':
+        return '#B2935B'; // Brown
+      case 'steel':
+        return '#737373'; // Gray
+      case 'ice':
+        return '#5CE1E6'; // Light blue
+      case 'ghost':
+        return '#3A2366'; // Purple
+      case 'dragon':
+        return '#BEDD8B'; // Purple
+      case 'dark':
+        return '#393838'; // Black
+      case 'flying':
+        return '#0097B2'; // Sky blue
+      default:
+        return '#AFAFAD'; // Gray (Color por defecto si el tipo no coincide)
+    }
+  }
 }
